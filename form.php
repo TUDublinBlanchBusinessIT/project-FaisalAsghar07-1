0b03c4b78b152801e0b096a5ecd6f475cd7c3718
unknown <b00138240@mytudublin.ie>
2023-12-06 16:45:33 +0000

    btns

--- form.php
+++ form.php
@@ -31,5 +31,25 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
                 </tr>
             </thead>
             <tbody>
+                <?php  
+                while ($user_data = mysqli_fetch_array($result)) {
+                    echo "<tr>";
+                    echo "<td>".$user_data['name']."</td>";
+                    echo "<td>".$user_data['mobile']."</td>";
+                    echo "<td>".$user_data['email']."</td>";    
+                    echo "<td style='float: right;'>
+                            <a href='edit.php?id=$user_data[id]' class='btn btn-warning btn-sm'>Edit</a>
+                            <a href='delete.php?id=$user_data[id]' class='btn btn-danger btn-sm'>Delete</a>
+                          </td>";
+                    echo "</tr>";        
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
