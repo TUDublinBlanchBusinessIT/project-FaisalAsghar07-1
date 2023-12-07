fefa99c29852bc52d74cc239ee00f0aebf72f100
unknown <b00138240@mytudublin.ie>
2023-12-07 17:51:47 +0000

    editpage

--- edit.php
+++ edit.php
@@ -1,3 +1,15 @@
 <?php
 include_once("config.php");
+if(isset($_POST['update']))
+$id = $_POST['id'];
+	
+	$name=$_POST['name'];
+	$mobile=$_POST['mobile'];
+	$email=$_POST['email'];
+    // update user data
+	$result = mysqli_query($mysqli, "UPDATE users SET name='$name',email='$email',mobile='$mobile' WHERE id=$id");
+	
+	// Redirect to homepage to display updated user in list
+	header("Location: form.php");
+    exit();
 ?>
