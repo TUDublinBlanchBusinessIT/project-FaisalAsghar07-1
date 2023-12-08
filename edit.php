7f01b2e87345ac4a3e11112eb162d801b06bd23e
unknown <b00138240@mytudublin.ie>
2023-12-08 12:43:18 +0000

    editWrok

--- edit.php
+++ edit.php
@@ -1,6 +1,7 @@
 <?php
 include_once("config.php");
 if(isset($_POST['update']))
+{
 $id = $_POST['id'];
 	
 	$name=$_POST['name'];
@@ -11,7 +12,7 @@ $id = $_POST['id'];
 	
 	// Redirect to homepage to display updated user in list
 	header("Location: form.php");
-    
+}
 ?>
 <?php
 $id = $_GET['id'];
@@ -23,3 +24,50 @@ while($user_data = mysqli_fetch_array($result))
 	$mobile = $user_data['mobile'];
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit User Data</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <a href="form.php" class="btn btn-primary">Home</a>
+        <br/><br/>
+
+        <form name="update_user" method="post" action="edit.php">
+            <div class="form-group row">
+                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" name="name" value="<?php echo $name; ?>">
+                </div>
+				</div>
+            <div class="form-group row">
+                <label for="email" class="col-sm-2 col-form-label">Email</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="email" name="email" value="<?php echo $email; ?>">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="mobile" class="col-sm-2 col-form-label">Mobile</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="mobile" name="mobile" value="<?php echo $mobile; ?>">
+                </div>
+            </div>
+            <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+            <div class="form-group row">
+                <div class="col-sm-10 offset-sm-2">
+                    <button type="submit" name="update" class="btn btn-success">Update</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
